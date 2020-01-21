94b745445c695ad62f1284c0eb9a6910c4618a78
Teemmer <dziga2000@gmail.com>
2020-01-21 15:37:18 +0200

    clear init project

--- quiz_project/quiz_project/settings.py
+++ quiz_project/quiz_project/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -118,3 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
